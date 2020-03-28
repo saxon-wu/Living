98826ae99b382a7643676f0b39e7cb2f61150fad
saxon-wu <64893914@qq.com>
2020-03-28 22:40:43 +0800

    Added ant-design-pro for background management and the reactjs for client side example

---
+++ ant/src/pages/user/list/data.d.ts
@@ -0,0 +1,41 @@
+export interface TableListItem {
+  key: number;
+  disabled?: boolean;
+  href: string;
+  avatar: string;
+  name: string;
+  title: string;
+  owner: string;
+  desc: string;
+  callNo: number;
+  status: number;
+  updatedAt: Date;
+  createdAt: Date;
+  progress: number;
+  id: number;
+  mobile: string;
+  username: string;
+  email: string | null;
+  disabled: boolean;
+}
+
+export interface TableListPagination {
+  total: number;
+  pageSize: number;
+  current: number;
+}
+
+export interface TableListData {
+  list: TableListItem[];
+  pagination: Partial<TableListPagination>;
+}
+
+export interface TableListParams {
+  sorter?: string;
+  status?: string;
+  name?: string;
+  desc?: string;
+  key?: number;
+  pageSize?: number;
+  currentPage?: number;
+}
